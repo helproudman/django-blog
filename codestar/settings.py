c2034a3a61a14602ec0ceffa9b701e03d38a48c4
Helen Proudman <helen.proudman@students.codeinstitute.net>
2024-09-10 09:14:15 +0000

    secret key set as an environment variable

--- codestar/settings.py
+++ codestar/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+    
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ab_vitn-f(4kn$-z^d69bahbp3bq77o*8!#y85g9a1l#ow=gmn'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
